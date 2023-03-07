2bf3c8567ee45a6d26a7d60cd27414e67127e4b8
subzero <shubham8111994@gmail.com>
2023-03-07 12:43:35 +0530

    added feature to display result using lambda

--- src/MathApp.java
+++ src/MathApp.java
@@ -3,15 +3,23 @@
 //Created functional interface
 interface IMathFunction{
     int calculate(int a, int b);
+    static void printResult(int a, int b, String functionName, IMathFunction obj){
+        System.out.println("Result of "+functionName+" is "+obj.calculate(a,b));
+    }
 }
 public class MathApp {
     public static void main(String[] args) {
-
+        //Lambda expression for math operations
         IMathFunction add = (a,b) -> a+b;
         IMathFunction subtract = (a,b) -> a-b;
         IMathFunction multiply = (a,b) -> a*b;
         IMathFunction divide = (a,b) -> a/b;
 
         System.out.println("Adding numbers "+ add.calculate(2,3));
+        System.out.println("Subtracting numbers "+ subtract.calculate(2,3));
+        System.out.println("Multiply numbers "+ multiply.calculate(2,3));
+        System.out.println("Divide numbers "+ divide.calculate(2,3));
+
+       IMathFunction.printResult(4,2,"addition",add);
     }
 }
